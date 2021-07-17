f1b1a6041cb95a605c7be85a05dbccd944c6944a
jeetpatel24 <friend.jeet24@gmail.com>
2021-07-17 17:51:46 +0530

    repository

--- online-store-example/src/main/java/com/javamaster/OnlineStoreExampleApplication.java
+++ online-store-example/src/main/java/com/javamaster/OnlineStoreExampleApplication.java
@@ -1,13 +1,30 @@
 package com.javamaster;
 
+import com.javamaster.entity.UserEntity;
+import com.javamaster.repository.UserEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class OnlineStoreExampleApplication {
+
+	@Autowired
+	private UserEntityRepository userEntityRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineStoreExampleApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void testRepository() {
+		UserEntity u = new UserEntity();
+		u.setAddress("address");
+		u.setName("name");
+		u.setPhone("phone");
+		u.setEmail("email");
+		userEntityRepository.save(u);
+	}
 }
---
+++ online-store-example/src/main/java/com/javamaster/repository/UserEntityRepository.java
@@ -0,0 +1,7 @@
+package com.javamaster.repository;
+
+import com.javamaster.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserEntityRepository extends JpaRepository<UserEntity, Integer> {
+}
